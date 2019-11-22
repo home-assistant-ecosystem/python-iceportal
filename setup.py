ca31a0b548a5a9d80fb5f5516abfb1fa60ea2072
Fabian Affolter <mail@fabian-affolter.ch>
2019-11-22 12:29:28 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Setup file for the ICE Portal data scraper."""
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme:
+    long_description = readme.read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='iceportal',
+    version='0.1.0',
+    description='Python client for getting data from the ICE Portal.',
+    long_description=long_description,
+    url='https://github.com/fabaff/python-iceportal',
+    download_url='https://github.com/fabaff/python-iceportal/releases',
+    author='Fabian Affolter',
+    author_email='fabian@affolter-engineering.ch',
+    license='MIT',
+    install_requires=['aiohttp', 'async_timeout'],
+    packages=['iceportal'],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+    ],
+)
