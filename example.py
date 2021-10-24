44a5acebe603f83e07108f1cc38449f9bfb1fb37
Fabian Affolter <mail@fabian-affolter.ch>
2021-10-24 15:36:56 +0200

    Update example

--- example.py
+++ example.py
@@ -1,24 +1,21 @@
 """Get the data from the ICE portal about the current trip."""
 import asyncio
 
-import aiohttp
-
 from iceportal import IcePortal
 
 
 async def main():
     """Sample code to retrieve the data from the ICE portal."""
-    async with aiohttp.ClientSession() as session:
-        trip = IcePortal(loop, session)
+    trip = IcePortal()
 
-        # Print details about the current rip
-        await trip.get_data()
+    # Print details about the current rip
+    await trip.get_data()
 
-        print("Train:", trip.train)
-        print("Next stop:", trip.next_stop)
-        print("Track:", trip.track)
-        print("Arrival time:", trip.arrival_time)
-
+    print("Train:", trip.train)
+    print("Next stop:", trip.next_stop)
+    print("Track:", trip.track)
+    print("Arrival time:", trip.arrival_time)
+    print("Current speed (km/h):", trip.train_speed)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
